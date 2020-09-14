0304edc7856ba64341c79257709b06fd9a4286ac
moth b <samtikka@gmail.com>
2020-09-14 14:41:00 +0300

    Testimessage

---
+++ MvxStarter.Core/Models/DogModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MvxStarter.Core.Models
+{
+    public class DogModel
+    {
+        public string Name { get; set; }
+
+        public string Bark { get; set; }
+    }
+}
